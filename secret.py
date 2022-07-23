5d8275da6941910d5240716f2f1f0817a0c31305
jeanstephan <43873091+jeanstephan@users.noreply.github.com>
2022-07-23 19:04:13 +0300

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 
 class Secret:
 
-    token = '' # discord bot token
+    token = 'MTAwMDQyOTgwNzI0MDIyMDcyMg.GOXdMR._-vuRgZ5cCl2w0p27cf3YTeRT9xN3KTA31FOm8' # discord bot token
     signal_channel_id = None # channel id for server the bot will be talking to
